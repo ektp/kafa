0623e90735110f4b89da76812a25ddbf3cd68b0f
Rolly Maulana Awangga <rolly@awang.ga>
2019-04-01 11:29:50 +0700

    simple

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ import config
 ser = serial.Serial(config.port, config.baudrate)
 
 while 1:
-	#data=ser.readline().rstrip('\n')
 	data=ser.readline()
-	#print data
-	#data=data.strip()
 	print(data)
-	#if data[:1]=="[":
-	#	print "\a"
-	#	trimdata = data.replace(" ","")
-	#	thedata = urlEncode16(trimdata)
-	#	uri = config.keyuri+'%input%ktp%'+trimdata
-	#	thedata = urlEncode16(uri)
-	#	webbrowser.open_new(config.host+thedata)
 
